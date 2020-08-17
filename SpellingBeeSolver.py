67e0d83ed5899c234e018abeb1fc89a2d94f570f
Thomas Park <mystifiedthomas@gmail.com>
2020-08-17 03:50:55 -0500

    Tweaking

--- SpellingBeeSolver.py
+++ SpellingBeeSolver.py
@@ -11,6 +11,12 @@ for x in range(7):
     letr = input("Please enter one letter, then press enter: ")
     oplst.append(letr.lower())
 
+biglst = []
+
+for elem in oplst:
+    for x in range(2):
+        biglst.append(elem)               
+                    
 print(""
 )
 keyletr = input("What is the key letter, please?: ")
@@ -23,20 +29,11 @@ print("")
 
 print("This may take a moment.")
 
+print("")
 
 wdlst = []
 
-worm = list(permutations(oplst, 4))
-worn = list(permutations(oplst, 5))
-woro = list(permutations(oplst, 6))
-worp = list(permutations(oplst, 7))
-worq = list(permutations(oplst, 8))
-worf = list(permutations(oplst, 9))
-wort = list(permutations(oplst, 10))
-worl = list(permutations(oplst, 10))
-wory = list(permutations(oplst, 10))
-
-wor = ""
+worm = list(permutations(biglst, 4))
 
 for elem in worm:
 
@@ -47,8 +44,13 @@ for elem in worm:
 
     if d.check(astr) and astr not in wdlst and keyletr in astr:
 
+        print(astr)
+
         wdlst.append(astr)
 
+worm = []
+
+worn = list(permutations(biglst, 5))
 
 for elem in worn:
 
@@ -59,62 +61,96 @@ for elem in worn:
 
     if d.check(astr) and astr not in wdlst and keyletr in astr:
 
+        print(astr)
+
         wdlst.append(astr)
+        
+worn = []
+
+woro = list(permutations(biglst, 6))
+
+for elem in woro:
+
+    astr = ""
+
+    for wor in elem:
+        astr += wor
+
+    if d.check(astr) and astr not in wdlst and keyletr in astr:
+
+        print(astr)
+
+        wdlst.append(astr)
+        
+woro = []
+
+worp = list(permutations(biglst, 7))
+
+for elem in worp:
+
+    astr = ""
+
+    for wor in elem:
+        astr += wor
+
+    if d.check(astr) and astr not in wdlst and keyletr in astr:
+
+        print(astr)
+
+        wdlst.append(astr)
+        
+worp = []
+
+worq = list(permutations(biglst, 8))
 
 for elem in worq:
 
     astr = ""
-    
+
     for wor in elem:
         astr += wor
 
     if d.check(astr) and astr not in wdlst and keyletr in astr:
 
+        print(astr)
+
         wdlst.append(astr)
+        
+worq = []
+
+worf = list(permutations(biglst, 9))
 
 for elem in worf:
 
     astr = ""
-    
+
     for wor in elem:
         astr += wor
 
     if d.check(astr) and astr not in wdlst and keyletr in astr:
 
+        print(astr)
+
         wdlst.append(astr)
+        
+worf = []
+
+wort = list(permutations(biglst, 10))
 
 for elem in wort:
 
     astr = ""
-    
+
     for wor in elem:
         astr += wor
 
     if d.check(astr) and astr not in wdlst and keyletr in astr:
 
-        wdlst.append(astr)
-
-for elem in worl:
-
-    astr = ""
-    
-    for wor in elem:
-        astr += wor
-
-    if d.check(astr) and astr not in wdlst and keyletr in astr:
+        print(astr)
 
         wdlst.append(astr)
-
-for elem in wory:
-
-    astr = ""
-    
-    for wor in elem:
-        astr += wor
-
-    if d.check(astr) and astr not in wdlst and keyletr in astr:
-
-        wdlst.append(astr)
+        
+wort = []
 
 print("")
 
